87833efe956fb42e7e5687be79b8891ab8824d15
Christopher Kim <chris.kim1990@gmail.com>
2020-01-27 15:48:17 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,8 @@
 def valid_move?(board, index)
-  def position_taken(array, index)
+  def position_taken?(array, index)
     if array[index] == " " || array[index] == "" || array[index] == nil
       return false
     else
       return true
     end
   end
-  
